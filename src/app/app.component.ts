15c82158d75fc8a5acd0075a994332968d38f124
AliTarani <a.tarani1999@gmail.com>
2024-03-18 11:59:42 +0330

    editor implemented normally.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'RTLcodeEditor';
+  editorOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
+    theme: 'dummy-light',
+    language: 'dummy',
+    minimap: { enabled: false },
+
+    overviewRulerLanes: 0,
+    hideCursorInOverviewRuler: true,
+    scrollbar: {
+      vertical: 'hidden',
+    },
+    overviewRulerBorder: false,
+    lineNumbers: 'off',
+    glyphMargin: false,
+
+    lineDecorationsWidth: 0,
+    lineNumbersMinChars: 0,
+  };
+  code: string = 'متن اول دوم نوشته سوم';
 }
